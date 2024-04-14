644c37bccf57fe3c1fe781b6d1c9efeaafee3c23
Heblishli <heblishli@MacBook-Air.local>
2024-04-14 07:05:19 +0200

    chore: forge init

--- Week-3/Week3.sol
+++ Week-3/Week3.sol
@@ -10,7 +10,8 @@ contract Week3 {
 
     function rationalAdd(ECPoint calldata A, ECPoint calldata B, uint256 num, uint256 den) public view returns (bool verified) {
         // modExp(a, -1, curve_order) == modExp(a, curve_order - 2, curve_order)
-        return mulmod(A.x, modExp(A.y, order-2, order)) + mulmod(B.x, modExp(B.y, order-2, order));
+        console.log(modExp());
+        //return mulmod(A.x, modExp(A.y, order-2, order)) + mulmod(B.x, modExp(B.y, order-2, order));
         // return true if the prover knows two numbers that add up to num/den
     }
 
