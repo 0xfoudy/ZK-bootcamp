374c1a5ac7cb00ad5a62d64acde2a5f958a7d9c4
Heblishli <heblishli@MacBook-Air.local>
2024-04-14 07:06:01 +0200

    chore: forge init

--- Week-3/Week3.sol
+++ Week-3/Week3.sol
@@ -1,18 +1,73 @@
 pragma solidity 0.8.20;
+import "forge-std/console.sol";
 
 contract Week3 {
-    constant uint256 order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
 
     struct ECPoint {
         uint256 x;
         uint256 y;
     }
 
-    function rationalAdd(ECPoint calldata A, ECPoint calldata B, uint256 num, uint256 den) public view returns (bool verified) {
-        // modExp(a, -1, curve_order) == modExp(a, curve_order - 2, curve_order)
-        console.log(modExp());
-        //return mulmod(A.x, modExp(A.y, order-2, order)) + mulmod(B.x, modExp(B.y, order-2, order));
-        // return true if the prover knows two numbers that add up to num/den
+    function expmod(uint base, uint e, uint m) public view returns (uint o) {
+        assembly {
+        // define pointer
+        let p := mload(0x40)
+        // store data assembly-favouring ways
+        mstore(p, 0x20)             // Length of Base
+        mstore(add(p, 0x20), 0x20)  // Length of Exponent
+        mstore(add(p, 0x40), 0x20)  // Length of Modulus
+        mstore(add(p, 0x60), base)  // Base
+        mstore(add(p, 0x80), e)     // Exponent
+        mstore(add(p, 0xa0), m)     // Modulus
+        if iszero(staticcall(sub(gas(), 2000), 0x05, p, 0xc0, p, 0x20)) {
+            revert(0, 0)
+        }
+        // data
+        o := mload(p)
+        }
+    }
+    
+    uint256 public constant order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+    uint256 public G_x = 55066263022277343669578718895168534326250603453777594175500187360389116729240;
+    uint256 public G_y = 32670510020758816978083085130507043184471273380659243275938904335757337482424;
+
+    function getInverse(uint256 toInverse) public returns (uint256 inverse) {
+        inverse = expmod(toInverse, order - 2, order); 
     }
 
+    // I know two rationals a,b that add up to num/den, i give you A, B, num and den
+    function rationalAdd(ECPoint memory A, ECPoint memory B, uint256 num, uint256 den) public returns (bool verified) {
+        
+        /*
+        // Gnum
+        uint256 ECNum_x = mulmod(G_x, num, order);
+        uint256 ECNum_y = mulmod(G_y, num, order);
+
+        // GDen
+        uint256 ECDen_x = mulmod(G_x, den, order);
+        uint256 ECDen_y = mulmod(G_y, den, order);
+
+        uint256 ECFraction_x = mulmod(ECNum_x, getInverse(ECDen_x), order);
+        uint256 ECFraction_y = mulmod(ECNum_y, getInverse(ECDen_y), order);
+*/
+
+        uint256 fraction = mulmod(num, getInverse(den), order);
+        uint256 ECFraction_x = mulmod(G_x, fraction, order);
+        uint256 ECFraction_y = mulmod(G_y, fraction, order);
+
+        return (addmod(A.x, B.x, order)) == ECFraction_x && (addmod(A.y, B.y, order)) == ECFraction_y;
+    }
+
+    // multiply matrix by s, verify that Ms = o
+    function matmul(uint256[] calldata matrix,
+                uint256 n, // n x n for the matrix
+                ECPoint[] calldata s, // n elements
+                uint256[] calldata o // n elements
+               ) public returns (bool verified) {
+
+                // revert if dimensions don't make sense or the matrices are empty
+                require(matrix.length > 0 && matrix.length == n && s.length == n && o.length == n);
+
+                // return true if Ms == o elementwise. You need to do n equality checks. If you're lazy, you can hardcode n to 3, but it is suggested that you do this with a for loop 
+    }
 }
