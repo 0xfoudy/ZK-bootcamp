a2c57e8619512763e6a7dec94f4ba17e421b238d
Heblishli <heblishli@MacBook-Air.local>
2024-04-14 07:08:09 +0200

    week 3

--- test/week3.t.sol
+++ test/week3.t.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.8.20;
 import "forge-std/console.sol";
 import {Test, console2} from "forge-std/Test.sol";
-import {Week3} from "../src/week3.sol";
+import {Week3} from "../Week-3/week3.sol";
 
 
 contract TestWeek3 is Test{
@@ -19,30 +19,30 @@ contract TestWeek3 is Test{
 
     }
 
-    function testAddFractions(uint256 a, uint256 b, uint256 num, uint256 den) public {
+    function testAddFractions(uint256 a, uint256 b, uint256 num, uint256 den) public returns (bool){
         // Ga
         Week3.ECPoint memory A = Week3.ECPoint(mulmod(G_x, a, order), mulmod(G_y, a, order));
         // Gb
         Week3.ECPoint memory B = Week3.ECPoint(mulmod(G_x, b, order), mulmod(G_y, b, order));
 
 
-        console.log(testCa.rationalAdd(A, B, num, den));
+        return testCa.rationalAdd(A, B, num, den);
     }
 
     function testH() public {
-        testAddFractions(1, 1, 4, 2);
-        testAddFractions(0, 2, 4, 2);
-        testAddFractions(2, 0, 4, 2);
-        testAddFractions(3, 0, 6, 2);
-        testAddFractions(0, 3, 6, 2);
-        testAddFractions(1, 2, 6, 2);
-        testAddFractions(2, 1, 6, 2);
+        assertTrue(testAddFractions(1, 1, 4, 2));
+        assertTrue(testAddFractions(0, 2, 4, 2));
+        assertTrue(testAddFractions(2, 0, 4, 2));
+        assertTrue(testAddFractions(3, 0, 6, 2));
+        assertTrue(testAddFractions(0, 3, 6, 2));
+        assertTrue(testAddFractions(1, 2, 6, 2));
+        assertTrue(testAddFractions(2, 1, 6, 2));
 
 
-        testAddFractions(12, 13, 100, 4);
-        testAddFractions(24, 1, 100, 4);
-        testAddFractions(1, 24, 100, 4);
-        testAddFractions(25, 0, 100, 4);
+        assertTrue(testAddFractions(12, 13, 100, 4));
+        assertTrue(testAddFractions(24, 1, 100, 4));
+        assertTrue(testAddFractions(1, 24, 100, 4));
+        assertTrue(testAddFractions(25, 0, 100, 4));
     }
 
 }
